4f23a88a4ac176bc06dd2c6e4bd5161ddea4c2f5
Juliette <julileeheehee@gmail.com>
2025-04-21 03:33:40 -0500

    Added organization.png to draw out how I'm going to organize my classes. Similar to particle and particleTrail from the previous lab

--- src/tiles.py
+++ src/tiles.py
@@ -1,5 +1,6 @@
 import pygame
 
+#-------------------------------Constants--------------------------------------------
 GRID_X = 2 # tiles
 GRID_Y = 2
 
@@ -7,20 +8,17 @@ TILESIZE = 64 # I made my tiles 64x64. Not sure why lmao
 
 SCALE = 3
 
-# Tile types w/ index
-BLANK_TILE = 0
-WATER_TILE = 1
-COASTLINE_TILE = 2
-GRASS_TILE = 3
+# Directions clockwise
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
 
-def loadImages():
-    tiles = [
-        pygame.transform.scale((pygame.image.load('images/3-tiles/blank_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE)),
-        pygame.transform.scale((pygame.image.load('images/3-tiles/water_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE)),
-        pygame.transform.scale((pygame.image.load('images/3-tiles/coastline_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE)),
-        pygame.transform.scale((pygame.image.load('images/3-tiles/grass_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE))
-    ]
-    return tiles
+# Tile types w/ index
+WATER_TILE = 0
+COASTLINE_TILE = 1
+GRASS_TILE = 2
+
 
 # Note: I'm doing this because I know I'll need it later
 tileEdges = {
@@ -29,8 +27,51 @@ tileEdges = {
     GRASS_TILE : ["grass", "grass", "grass", "grass"]
 }
 
-# makes the grid
-grid = []
+#---------------------------Classes (will move later)-----------------------------------------
+class Tiles:
+    def __init__(self, tileIndex, x, y):
+        self.index = tileIndex
+        self.gridcopy = [[0 for row in range(GRID_X)] for col in range(GRID_Y)]
+        self.gridlocation = self.gridcopy[x][y]
+
+        self.possibilities = [WATER_TILE, COASTLINE_TILE, GRASS_TILE]
+        self.entropy = len(self.possibilities) #should be 3 at the start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def loadImages():
+    tileImages = [
+        pygame.transform.scale((pygame.image.load('images/3-tiles/water_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE)),
+        pygame.transform.scale((pygame.image.load('images/3-tiles/coastline_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE)),
+        pygame.transform.scale((pygame.image.load('images/3-tiles/grass_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE))
+    ]
+    return tileImages
 
 def main():
     pygame.init()
@@ -38,8 +79,10 @@ def main():
     tilesize = TILESIZE * SCALE
     screen = pygame.display.set_mode((GRID_X * tilesize, GRID_X * tilesize))
     clock = pygame.time.Clock()
-    tiles = loadImages()
+    tileImages = loadImages()
+    blank_tile = pygame.transform.scale((pygame.image.load('images/3-tiles/blank_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE))
 
+    collapsed = False
     running = True
 
     while running:
@@ -53,10 +96,14 @@ def main():
         
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if i == 1:
-                    pygame.Surface.blit(screen, tiles[BLANK_TILE], (i*tilesize, j*tilesize))
-                else:
-                    pygame.Surface.blit(screen, tiles[GRASS_TILE], (i*tilesize, j*tilesize))
+                pygame.Surface.blit(screen, blank_tile, (i*tilesize, j*tilesize))
+        
+        grid[0][0] = pygame.Surface.blit(screen, tileImages[0], (0, 0))
+
+        print("[0][0] entropy: ")
+        print("[0][1] entropy: ")
+        print("[1][0] entropy: ")
+        print("[1][1] entropy: ")
 
 
 
