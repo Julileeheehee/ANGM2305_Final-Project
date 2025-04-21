8d6a4553c5df3d475f266870eff9593f22872765
Juliette <julileeheehee@gmail.com>
2025-04-21 01:50:32 -0500

    Made a 2x2 grid with only water tiles. I'm not sure why I'm getting a warning when I made my own tiles on Photoshop

--- src/tiles.py
+++ src/tiles.py
@@ -7,11 +7,18 @@ TILESIZE = 64 # I made my tiles 64x64. Not sure why lmao
 
 SCALE = 3
 
-# Tile types
-# The number is the id of the image(idk)
+# Tile types w/ index
 WATER_TILE = 0
 COASTLINE_TILE = 1
 GRASS_TILE = 2
+
+def loadImages():
+    tiles = [
+        pygame.transform.scale((pygame.image.load('images/3-tiles/water_tile.png').convert_alpha()), (SCALE*TILESIZE, SCALE*TILESIZE)),
+        pygame.image.load('images/3-tiles/coastline_tile.png').convert_alpha(),
+        pygame.image.load('images/3-tiles/grass_tile.png').convert_alpha(),
+    ]
+    return tiles
 
 # Note: I'm doing this because I know I'll need it later
 tileEdges = {
@@ -20,18 +27,47 @@ tileEdges = {
     GRASS_TILE : ["grass", "grass", "grass", "grass"]
 }
 
+# makes the grid
+grid = []
 
 def main():
     pygame.init()
     pygame.display.set_caption("3 Tile Test")
-    screen = pygame.display.set_mode((GRID_X * TILESIZE * SCALE, GRID_X * TILESIZE * SCALE))
+    tilesize = TILESIZE * SCALE
+    screen = pygame.display.set_mode((GRID_X * tilesize, GRID_X * tilesize))
     clock = pygame.time.Clock()
+    tiles = loadImages()
+
     running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        #---------------add code below---------------
+        for row in range(GRID_X):
+            for col in range(GRID_Y):
+                pygame.Surface.blit(screen, tiles[0], (row*tilesize, col*tilesize))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         pygame.display.flip()
     pygame.quit()
