235109ea36f07d9df4db4ae6ce86a51115c20226
Juliette <julileeheehee@gmail.com>
2025-04-20 22:28:45 -0500

    Made a 3x3 grid, but had to move the left panel to (0,0) since I was a little too dumb to figure out how to move the grid to the correct spot

--- src/main.py
+++ src/main.py
@@ -16,10 +16,17 @@ class Panels:
         self.display_surface = pygame.display.get_surface()
 
     def update(self):
-        self.display_surface.blit(self.surface_left, (PADDING, PADDING*2)) # Purple in picture
+        self.display_surface.blit(self.surface_left, (0,0)) # Purple in picture
         self.display_surface.blit(self.surface_right, (PADDING*2 + GRID_WIDTH, PADDING*2)) # light blue in picture
 
 
+
+def draw_grid(screen):
+    tile_size = GRID_WIDTH//3
+    for row in range(0, GRID_WIDTH - 1, tile_size):
+        for col in range(0, GRID_HEIGHT - 1, tile_size):
+            rect = pygame.Rect(row, col, tile_size, tile_size)
+            pygame.draw.rect(screen, BLACK, rect, 2)
 
 
 
@@ -40,8 +47,15 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        #backgrounds
         screen.fill(GRAY)
         panels.update()
+
+        #draw
+        draw_grid(screen)
+
+
+        #flip display
         pygame.display.flip()
         clock.tick()
 
