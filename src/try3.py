a9e19360fbb0dddc760d4d3f31df9dc145cd70b7
Juliette <julileeheehee@gmail.com>
2025-04-28 23:43:46 -0500

    The neighbor coordinates are calculated. Next, I'll try to pick a neighbor at random and assign to a random tile for now

--- src/try3.py
+++ src/try3.py
@@ -40,8 +40,44 @@ def grid4():
 
     print('\n'.join(map(' '.join, grid)))
 
+def grid5():
+    empty_cell = "n"
+    tiles = ["W", "C", "G"]
+    grid = [[0 for i in range(3)] for j in range(3)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            grid[i][j] = empty_cell
+    
+    row = random.randint(0,2)
+    col = random.randint(0,2)
+    print(f"Coordinates: {row},{col}")
+    print(findNeighbors(row, col, 3, 3))
+    grid[row][col] = random.choice(tiles)
+
+    print('\n'.join(map(' '.join, grid)))
+
+def findNeighbors(coord_row, coord_col, grid_row, grid_col):
+    north = "none"
+    if coord_row != 0:
+        north = f"{coord_row - 1},{coord_col}"
+
+    east = "none"
+    if coord_col != grid_col:
+        east = f"{coord_row},{coord_col + 1}"
+
+    south = "none"
+    if coord_row != grid_row:
+        south = f"{coord_row + 1},{coord_col}"
+
+    west = "none"
+    if coord_col != 0:
+        west = f"{coord_row},{coord_col - 1}"
+    
+    text = f"\nNeighbor locations:\n\nnorth = {north}\neast = {east}\nsouth = {south}\nwest = {west}\n"
+    return text
+
 def main():
-    grid4()
+    grid5()
 
 if __name__ == "__main__":
     main()
