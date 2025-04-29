722fe3e59e7a4f84c41eb5d58ecce196ed62263a
Juliette <julileeheehee@gmail.com>
2025-04-29 00:37:35 -0500

    I am able to choose a random direction from the first tile and assign it "X". Next, I'll attempt to constrain the randomness and create rules for the tiles

--- src/try3.py
+++ src/try3.py
@@ -52,32 +52,72 @@ def grid5():
     col = random.randint(0,2)
     print(f"Coordinates: {row},{col}")
     print(findNeighbors(row, col, 3, 3))
+
     grid[row][col] = random.choice(tiles)
 
     print('\n'.join(map(' '.join, grid)))
 
-def findNeighbors(coord_row, coord_col, grid_row, grid_col):
-    north = "none"
-    if coord_row != 0:
-        north = f"{coord_row - 1},{coord_col}"
-
-    east = "none"
-    if coord_col != grid_col:
-        east = f"{coord_row},{coord_col + 1}"
-
-    south = "none"
-    if coord_row != grid_row:
-        south = f"{coord_row + 1},{coord_col}"
-
-    west = "none"
-    if coord_col != 0:
-        west = f"{coord_row},{coord_col - 1}"
+def grid6():
+    empty_cell = "n"
+    tiles = ["W", "C", "G"]
+    grid = [[0 for i in range(3)] for j in range(3)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            grid[i][j] = empty_cell
     
-    text = f"\nNeighbor locations:\n\nnorth = {north}\neast = {east}\nsouth = {south}\nwest = {west}\n"
-    return text
+    #row = random.randint(0,2)
+    #col = random.randint(0,2)
+    row = 1
+    col = 1
+    print(f"Coordinates: {row},{col}")
+    neighbors = findNeighbors(row, col, 3, 3)
+    randomNeighbor = assignRandomNeighbor(neighbors)
+    
+
+    firstTile = random.choice(tiles)
+    grid[row][col] = firstTile
+
+    grid[randomNeighbor[0]][randomNeighbor[1]] = "X"
+
+    print('\n'.join(map(' '.join, grid)))
+
+def findNeighbors(coord_row, coord_col, grid_row, grid_col):
+    north = None
+    if coord_row != 0:
+        north = [coord_row - 1,coord_col]
+
+    east = None
+    if coord_col != grid_col - 1:
+        east = [coord_row, coord_col + 1]
+
+    south = None
+    if coord_row != grid_row - 1:
+        south = [coord_row + 1,coord_col]
+
+    west = None
+    if coord_col != 0:
+        west = [coord_row,coord_col - 1]
+    
+    listofNeighbors = [north,east,south,west]
+    return listofNeighbors
+
+
+def assignRandomNeighbor(listofNeighbors):
+    #north = listofNeighbors[0]
+    #east = listofNeighbors[1]
+    #south = listofNeighbors[2]
+    #west = listofNeighbors[3]
+
+    choice = []
+    for item in listofNeighbors:
+        if item != None:
+            choice.append(item)
+    randomNeighbor = random.choice(choice)
+    return randomNeighbor
+
 
 def main():
-    grid5()
+    grid6()
 
 if __name__ == "__main__":
     main()
