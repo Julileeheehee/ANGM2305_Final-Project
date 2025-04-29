443be11e9b98df5629f615ed4a8182d8f13589cd
Juliette <julileeheehee@gmail.com>
2025-04-28 23:15:54 -0500

    Made every cell empty for now. Will try to assign random tiles to the cell next

--- src/try3.py
+++ src/try3.py
@@ -7,9 +7,16 @@ def grid():
             grid[i][j] = f"{i}{j} "
     print('\n'.join(map(''.join, grid))) # map() applies a function to each item in an iterable. .join combines all items into one string. In this case, each row is separated by a newline
 
+def grid2():
+    empty_cell = "n"
+    grid = [[0 for row in range(3)] for col in range(3)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            grid[i][j] = f"{empty_cell} "
+    print('\n'.join(map(''.join, grid)))
 
 def main():
-    grid()
+    grid2()
 
 if __name__ == "__main__":
     main()
