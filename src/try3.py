4072b37f80315657be208bf66b1911179767a0d8
Juliette <julileeheehee@gmail.com>
2025-04-29 01:58:18 -0500

    I kept iterating manually until something went wrong. I need to check all the neighbors properly

--- src/try3.py
+++ src/try3.py
@@ -57,15 +57,8 @@ def grid5():
 
     print('\n'.join(map(' '.join, grid)))
 
-tilerules = {
-    'W': ["W", "C"],
-    'C': ["W", "C", "G"],
-    'G': ["C", "G"]
-}
-
 def grid6():
     empty_cell = "n"
-    tiles = ["W", "C", "G"]
     grid = [[0 for i in range(3)] for j in range(3)]
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -86,6 +79,64 @@ def grid6():
     grid[row][col] = firstTile
 
     grid[randomNeighbor[0]][randomNeighbor[1]] = nextTile
+
+    if any('n' in sublist for sublist in grid):
+        print("Still not collapsed")
+
+    print('\n'.join(map(' '.join, grid)))
+
+tilerules = {
+    'W': ["W", "C"],
+    'C': ["W", "C", "G"],
+    'G': ["C", "G"]
+}
+
+tiles = ["W", "C", "G"]
+
+def grid7():
+    
+    empty_cell = "n"
+    grid = [[0 for i in range(3)] for j in range(3)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            grid[i][j] = empty_cell
+    row = random.randint(0,2)
+    col = random.randint(0,2)
+    #row = 1
+    #col = 1
+
+    curr_row = row
+    curr_col = col
+
+    firstTile = random.choice(tiles)
+    currTile = firstTile
+
+    #while any('n' in sublist for sublist in grid):
+    grid[curr_row][curr_col] = currTile
+
+    neighbors = findNeighbors(curr_row, curr_col, 3, 3)
+    randomNeighbor = assignRandomNeighbor(neighbors, grid)
+    nextTile = assignRandomTile(currTile)
+    grid[randomNeighbor[0]][randomNeighbor[1]] = nextTile
+
+    #handles 2nd iteration
+    curr_row = randomNeighbor[0]
+    curr_col = randomNeighbor[1]
+    currTile = nextTile
+    neighbors = findNeighbors(curr_row, curr_col, 3, 3)
+    randomNeighbor = assignRandomNeighbor(neighbors, grid)
+    nextTile = assignRandomTile(currTile)
+    grid[randomNeighbor[0]][randomNeighbor[1]] = nextTile
+
+    curr_row = randomNeighbor[0]
+    curr_col = randomNeighbor[1]
+    currTile = nextTile
+    neighbors = findNeighbors(curr_row, curr_col, 3, 3)
+    randomNeighbor = assignRandomNeighbor(neighbors, grid)
+    nextTile = assignRandomTile(currTile)
+    grid[randomNeighbor[0]][randomNeighbor[1]] = nextTile
+
+        
 
     print('\n'.join(map(' '.join, grid)))
 
@@ -109,17 +160,23 @@ def findNeighbors(coord_row, coord_col, grid_row, grid_col):
     listofNeighbors = [north,east,south,west]
     return listofNeighbors
 
-def assignRandomNeighbor(listofNeighbors):
+def assignRandomNeighbor(listofNeighbors, grid):
     #north = listofNeighbors[0]
     #east = listofNeighbors[1]
     #south = listofNeighbors[2]
     #west = listofNeighbors[3]
-
+    copy_grid = grid
+    print(copy_grid)
     choice = []
     for item in listofNeighbors:
         if item != None:
-            choice.append(item)
+            i = item[0]
+            j = item[1]
+            if copy_grid[i][j] == 'n':
+                choice.append(item)
+    #print(choice)
     randomNeighbor = random.choice(choice)
+    #print(randomNeighbor)
     return randomNeighbor
 
 def assignRandomTile(firstTile):
@@ -130,7 +187,7 @@ def assignRandomTile(firstTile):
 
 
 def main():
-    grid6()
+    grid7()
 
 if __name__ == "__main__":
     main()
