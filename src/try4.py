f869b368c21f088e31fe9879f1870a003fe2be9b
Juliette <julileeheehee@gmail.com>
2025-04-30 02:53:56 -0500

    I'm ignoring the class for now. I think I got confused

--- src/try4.py
+++ src/try4.py
@@ -13,41 +13,104 @@ EAST = 1
 WEST = 2
 SOUTH = 3
 
+
+
 class Cell():
     def __init__(self):
+        self.blank = "n"
+        self.tile = self.blank
         self.possibilities = TILES # Should start out with 3 in this case
         self.entropy = len(self.possibilities)
     
     def getEntropy(self):
         return str(self.entropy)
+    
+    def setToBlank(self):
+        return self.blank
+    
+    def collapseCell(self, pickedTile):
+        self.tile = pickedTile
+        self.entropy = 0
+        return self.tile
+    
+    def showEntropyNum(self):
+        self.tile = self.getEntropy()
+        return self.tile
 
-def start_over():
-    print("\nMain Grid:")
-    new_tile_grid()
-    print("------------------")
-    print("Entropy Grid:")
-    new_entropy_grid()
 
-def new_tile_grid():
-    empty_cell = "n"
+"""def grid():
+    #empty_cell = "n"
+    
     grid = [[0 for i in range(3)] for j in range(3)]
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            cell = Cell()
+            grid[i][j] = cell.setToBlank()
+    print('\n'.join(map(' '.join, grid)))
+
+    entropy_grid = grid
+    for i in range(len(entropy_grid)):
+       for j in range(len(entropy_grid[i])):
+
+            entropy_grid[i][j] = cell.showEntropyNum()
+    print('\n'.join(map(' '.join, entropy_grid)))
+
+    first_coord = pickFirstCell()
+    first_tile = pickFirstTile()
+    first_row = first_coord[0]
+    first_col = first_coord[1]
+
+    grid[first_row][first_col] = cell.collapseCell(first_tile)
+    print('\n'.join(map(' '.join, grid)))"""
+    
+def grid():
+    
+    
+    grid = [[0 for i in range(3)] for j in range(3)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            empty_cell = "n"
             grid[i][j] = empty_cell
-
     print('\n'.join(map(' '.join, grid)))
 
-def new_entropy_grid():
-    grid = [[0 for i in range(3)] for j in range(3)]
+    
+    
+
+    first_coord = pickFirstCell()
+    first_tile = pickFirstTile()
+    first_row = first_coord[0]
+    first_col = first_coord[1]
+
+    grid[first_row][first_col] = first_tile
+    print('\n'.join(map(' '.join, grid)))
+    showentropy(grid)
+    
+def showentropy(grid):
+    entropy = ""
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            grid[i][j] = Cell().getEntropy()
-
+       for j in range(len(grid[i])):
+            if grid[i][j] == "n":
+                entropy = 3
+            else:
+                entropy = 0
+            grid[i][j] = str(entropy)
     print('\n'.join(map(' '.join, grid)))
+
+def pickFirstCell():
+    row = int(input("Row? (0, 1, 2): "))
+    col = int(input("Column? (0, 1, 2): "))
+
+    first_coordinate = [row, col]
+    return first_coordinate
+
+def pickFirstTile():
+    first_tile = input("Tile? (W, C, G): ")
+    return first_tile
 
 
 def main():
-    start_over()
+    grid()
+
 
 if __name__ == "__main__":
     main()
