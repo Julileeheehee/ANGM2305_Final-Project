7db25a34518626c3f32ff1dbe605ed536b79119c
Juliette <julileeheehee@gmail.com>
2025-05-11 04:19:05 -0500

    Now I'm starting to work on Pygame. I'll work on getting a grid made and making it scale with different grid sizes with a slider

--- src/try7.py
+++ src/try7.py
@@ -1,52 +1,27 @@
+import pygame
 from enum import Enum
 import random
 
-# (done) Step 1: Initialize grid, cells, and all the tiles
-# (done) Step 2: Pick the first spot and the tile to collapse to
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-# (done) Step 3: Make a list of neighbors of collapsed cells (save as a tuple)
-# Step 4: Grab the , and remove tiles according to rules
-# Step 5: Make a dictionary of entropy keys with location values of the neighbors (will be updated a lot)
-# Step 6: Update entropy for grid via # tiles in cell
-# Step 7: Pick a random location from the smallest key
-# Step 8: If that cell has two tiles, pick a random tile
-# Step 9: Collapse that cell and start back at step 3
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (77, 77, 77)
 
-# Step 10: If there are no options available (definitely possible), start over with with the inputs of step 2.
-# Step 11: When the len(list of collapsed cells) == grid row x grid col, the map is complete
-
-
-###### I basically learned that it's SUPER IMPORTANT to include the types of the parameters, so i'm doing my best to include it all the time#############
-
-# ANSI escape sequences:-----------------------------------
-BRIGHT_GREEN = '\033[92m'
-BRIGHT_YELLOW = '\033[93m'
-BRIGHT_BLUE = '\033[94m'
-
-
-RESET = '\033[0m' # called to return to standard terminal text color
-#------------------------------------------------------------------
-
-
-
-
-
-class TileType(Enum): # This handles the tile types without making a class. Enum is super useful for handling nonchangable stuff. This helps me reduce checking with if/else statements
-    WATER = 'W'
+class TileType(Enum):
     COAST = 'C'
     GRASS = 'G'
 
 
 class Tile:
     def __init__(self):
-        self.tile_type : TileType | None = None #Basically, the tile type is either a type or none. The default is none because it hasn't been collapsed yet
-        self.available_tiles: set[TileType] = set() # A set is basically a dictionary, but just the keys. Like a list, but cannot be indexed. A set is initialized with () but uses {}
-
+        self.tile_type : TileType | None = None
+        self.available_tiles: set[TileType] = set()
 
     def set_available_tiles(self, north_neighbor, east_neighbor, south_neighbor, west_neighbor):
-        #self.available_tiles: set[TileType] = set()
         for tile_type in TileType:
-            self.available_tiles.add(tile_type) # no need to worry about duplicates
+            self.available_tiles.add(tile_type)
         
         self.update_tiles_from_neighbor(north_neighbor)
         self.update_tiles_from_neighbor(east_neighbor)
@@ -54,9 +29,10 @@ class Tile:
         self.update_tiles_from_neighbor(west_neighbor)
     
     def update_tiles_from_neighbor(self, neighbor):
-        if neighbor and neighbor.tile_type: #checks if these exist and are not None
+        #checks if these exist and are not None
+        if neighbor and neighbor.tile_type: 
             if neighbor.tile_type == TileType.WATER:
-                self.available_tiles &= set([TileType.WATER, TileType.COAST]) #learned a new assignment operator. Basically I'm combining two sets, the duplicates will be removed anyways because it's a set
+                self.available_tiles &= set([TileType.WATER, TileType.COAST])
             elif neighbor.tile_type == TileType.COAST:
                 self.available_tiles &= set([TileType.WATER, TileType.COAST, TileType.GRASS])
             elif neighbor.tile_type == TileType.GRASS:
@@ -78,18 +54,14 @@ def print_grid(grid: list[list[Tile | None]]):
     for col in grid:
         for row in col:
             if row.tile_type: # if it exists/not None
-                print(row.tile_type.value, end=" ") #enum.value just returns the value, so row.WATER will return a value of W
+                print(row.tile_type.value, end=" ")
             else:
                 print("-", end=" ")
         print()
     print()
 
 def determine_next_neighbor_coordinates(grid: list[list[Tile | None]], row: int, col: int):
-    # Take the grid, the location coordinates (x, y), and find the neighbors
     list_of_next_coordinates: list[tuple[int, int]] = []
-
-    #determine north, east, south, west neighbors
-    #Must be None
     
     #north
     if col - 1 >= 0 and grid[row][col - 1] and grid[row][col - 1].tile_type == None:
@@ -110,7 +82,7 @@ def get_tile_from_coordinates(grid, row, col):
     # check if the grid at the coordinate is not out of bounds
     if 0 <= row < len(grid) and 0 <= col < len(grid[0]):
         return grid[row][col]
-    else: # OoB
+    else:
         return None
 
 def fill_grid(grid: list[list[Tile | None]], starting_row: int, starting_col: int, starting_tile: TileType):
@@ -118,12 +90,6 @@ def fill_grid(grid: list[list[Tile | None]], starting_row: int, starting_col: in
     grid[starting_row][starting_col].tile_type = starting_tile
 
     next_neighbor_coordinates: list[tuple[int, int]] = determine_next_neighbor_coordinates(grid, starting_row, starting_col)
-
-    #while the next_neighbor_coordinates has places to move to,
-    #   loop through each location in the list
-    #   grab the tile
-    #   update the types it could be
-    #   and calculate its entropy
 
     while next_neighbor_coordinates:
         list_of_low_entropy_coordinates: list[tuple[int,int]] = []
@@ -142,18 +108,15 @@ def fill_grid(grid: list[list[Tile | None]], starting_row: int, starting_col: in
 
                 entropy = tile.get_entropy()
 
-                # From here: add the coordinates of the tiles with the lowest entropy
                 lowest_entropy = 3 # 3 is the highest entropy
                 if entropy < lowest_entropy: 
-                    list_of_low_entropy_coordinates.clear() # removes the old coordinates that have higher entropy numbers
+                    list_of_low_entropy_coordinates.clear()
                     list_of_low_entropy_coordinates.append(coordinate)
-                    lowest_entropy = entropy # Sets the lowest to the new entropy
-                elif entropy == lowest_entropy: # basically we need to try to get this down to 1 or 2
+                    lowest_entropy = entropy
+                elif entropy == lowest_entropy:
                     list_of_low_entropy_coordinates.append(coordinate)
         
-        # Pick a random coordinate fromm the list
         coordinate_to_move_to = random.choice(list_of_low_entropy_coordinates)
-        # Remove that coordinate from next_neighbor_coordinates
         next_neighbor_coordinates.remove(coordinate_to_move_to)
 
         move_to_row, move_to_col = coordinate_to_move_to
@@ -162,17 +125,41 @@ def fill_grid(grid: list[list[Tile | None]], starting_row: int, starting_col: in
         next_neighbor_coordinates.extend(determine_next_neighbor_coordinates(grid, move_to_row, move_to_col))
 
 def main():
-    num_of_rows = int(input("How many rows would you like?: "))
-    num_of_cols = int(input("How many columns would you like?: "))
-    grid: list[list[Tile | None]] = [[Tile() for col in range(num_of_cols)] for row in range(num_of_rows)] # I learned that | means or, so I can make it a tile or none!
-    print_grid(grid)
+    #num_of_rows = int(input("How many rows would you like?: "))
+    #num_of_cols = int(input("How many columns would you like?: "))
+    #grid: list[list[Tile | None]] = [[Tile() for col in range(num_of_cols)] for row in range(num_of_rows)]
+    #print_grid(grid)
 
-    starting_row = int(input(f"Row? (0-{num_of_rows}): "))
-    starting_col = int(input(f"Col? (0-{num_of_cols}): "))
-    starting_tile = TileType(input("Tile? (W, C, G): "))
+    #starting_row = int(input(f"Row? (0-{num_of_rows}): "))
+    #starting_col = int(input(f"Col? (0-{num_of_cols}): "))
+    #starting_tile = TileType(input("Tile? (W, C, G): "))
 
-    fill_grid(grid, starting_row, starting_col, starting_tile)
-    print_grid(grid)
+    #fill_grid(grid, starting_row, starting_col, starting_tile)
+    #print_grid(grid)
+    pygame.init()
+    pygame.display.set_caption("Wave Function Collapse")
+    resolution = (SCREEN_WIDTH, SCREEN_HEIGHT)
+    screen = pygame.display.set_mode(resolution)
+    clock = pygame.time.Clock()
+
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        #backgrounds
+        screen.fill(GRAY)
+
+
+
+
+        #flip display
+        pygame.display.flip()
+        clock.tick()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
